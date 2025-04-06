9dbb57035466c8a47828d5d38211a3d0debb1d4d
pabloBernini <chwast3712@gmail.com>
2025-04-06 18:23:52 +0200

    Working base for navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +57,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.navigation.compose.v270) // Or latest
+    implementation(libs.material3)
+    implementation(libs.ui)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.runtime.ktx.v270)
+    implementation(libs.androidx.activity.compose.v182)
+    implementation(libs.androidx.compose.bom.v20230800)
 }
--- app/src/main/java/com/example/dogsapp/MainActivity.kt
+++ app/src/main/java/com/example/dogsapp/MainActivity.kt
@@ -3,46 +3,72 @@ package com.example.dogsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.dogsapp.ui.theme.DogsAppTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-
         setContent {
-            DogsAppTheme {
-                    DogsList()
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    NavigationExample()
                 }
             }
         }
     }
-
+}
 
 @Composable
-fun DogsList() {
-    val dogNames = arrayOf("Azor", "Burek", "Reksio", "Lesi", "Szarik")
+fun NavigationExample() {
+    val navController = rememberNavController()
 
-    LazyColumn {
-        item {
-            Text(text = "Lista piesków")
-        }
-        items(dogNames.size) { index ->
-            Text(text = dogNames[index])
+    NavHost(navController = navController, startDestination = "screen1") {
+        composable("screen1") { Screen1(navController) }
+        composable("screen2") { Screen2(navController) }
+    }
+}
+
+@Composable
+fun Screen1(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Screen 1")
+        Button(onClick = { navController.navigate("screen2") }) {
+            Text("Go to Screen 2")
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DogsAppTheme {
-        DogsList()
+fun Screen2(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Screen 2")
+        Button(onClick = { navController.navigate("screen1") }) {
+            Text("Go back to Screen 1")
+        }
     }
+}
+@Composable
+fun TestComposable(){
+    Text("Testing")
 }
